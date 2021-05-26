0f6730f064c66910953e0981bcee16420ae71d9a
Espen Nordli <espen.nordli@tietoevry.com>
2021-05-26 15:34:00 +0200

    Adding check-of-financing-service

---
+++ check-of-financing-service/src/main/java/com/example/checkoffinancingservice/CheckOfFinancingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkoffinancingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckOfFinancingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckOfFinancingServiceApplication.class, args);
+    }
+
+}
